dfecdeab0accf407b9cd661a13eddcda08611c5e
Ayush-dev-45 <ayushneo45@gmail.com>
2026-02-19 00:13:30 +0530

    added movies service

--- go-movies-crud/main.go
+++ go-movies-crud/main.go
@@ -63,7 +63,19 @@ func createMovie(w http.ResponseWriter, r *http.Request){
 }
 
 func updateMovie(w http.ResponseWriter, r *http.Request){
-
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range movies {
+		if item.ID == params["id"]{
+			movies = append(movies[:index], movies[index+1:]... )
+		}
+		var movie Movie
+		_ = json.NewDecoder(r.Body).Decode(&movie)
+		movie.ID=params["id"]
+		movies = append(movies, movie)
+		json.NewEncoder(w).Encode(movie)
+		return
+	}
 }
 
 func main() {
@@ -76,6 +88,7 @@ func main() {
 	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
 	r.HandleFunc("/movies", createMovie).Methods("POST")
 	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
 
 	fmt.Printf("Server started at port 8080\n")
 	log.Fatal(http.ListenAndServe(":8080", r))
